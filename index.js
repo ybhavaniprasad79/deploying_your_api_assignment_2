2e5d219760883777a769cbfa018ea2bd2c8e9a25
tony <ybhavaniprasad79@gmail.com>
2025-02-17 17:02:31 +0530

    done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { resolve } = require('path');
+require('dotenv').config();
 
 const app = express();
 const port = 3010;
@@ -8,6 +9,15 @@ app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+
+app.get('/admin', (req, res) => {
+  if (process.env.IS_ADMIN === 'true') {
+    res.send({ message: "Welcome, Admin!", data: ["Admin Data 1", "Admin Data 2"] });
+  } else {
+    res.send({ message: "Welcome, User!", data: ["User Data 1", "User Data 2"] });
+  }
 });
 
 app.listen(port, () => {
